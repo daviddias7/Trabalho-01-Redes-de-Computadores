39983eacbc66a5332c6c20f3270bf3fed40f05f7
David Dias <67483023+daviddias7@users.noreply.github.com>
2022-10-17 16:04:36 +0000

    Adiciona nome de usuário a client.py

--- client.py
+++ client.py
@@ -1,12 +1,14 @@
 import socket
 
 addr = ('localhost', 8001)
+username = "client"
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.connect(addr)
 while True:
     msg = input()
-    s.sendall(bytes(msg, 'utf-8'))
+    s.sendall(bytes(username + ": " + msg, 'utf-8'))
     msg = s.recv(1024)
     if msg.decode('utf-8') == '@':
         break
