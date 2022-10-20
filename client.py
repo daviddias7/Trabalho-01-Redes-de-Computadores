cfeac6232776ff54d40a3edf8c7a52c85d27b8ef
VitorFCM <vitor.carreramiguel@gmail.com>
2022-10-20 16:18:18 -0300

    Servidor pode morrer e os clientes nao vao quebrar
    Mais uma thread foi criada para receber o input do usuario

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+import os
 
 addr = ('localhost', 8001)
 username = input("Digite seu nome: ")
@@ -7,7 +8,15 @@ username = input("Digite seu nome: ")
 def aguarda_mensagem(s):
     while True:
         msg = (s.recv(1024)).decode('utf-8')
+        if not msg: break
         print(msg)
+    print("Ocorreu um erro com o servidor")
+    return
+
+def envia_mensagem(s):
+    while True:
+        msg = input()
+        s.sendall(bytes(msg, 'utf-8'))
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -21,13 +30,17 @@ except:
     print("Ocorreu um problema durante a tentativa de conexao.")
     quit()
 
-thread_aguarda_mensagem = threading.Thread(target = aguarda_mensagem, args=(s,))
-thread_aguarda_mensagem.start()
+thread_aguarda_msg = threading.Thread(target = aguarda_mensagem, args=(s,))
+thread_aguarda_msg.start()
 
+
+thread_envia_msg = threading.Thread(target = envia_mensagem, args=(s,))
+thread_envia_msg.start()
 
 s.sendall(bytes(username, 'utf-8'))
 
-while True:
-    msg = input()
-    s.sendall(bytes(msg, 'utf-8'))
+while thread_aguarda_msg.is_alive():
+    pass
+
 s.close()
+os._exit(1)
