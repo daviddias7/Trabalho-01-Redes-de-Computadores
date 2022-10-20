9d6358832163e358a02a4b8e49d38f03bfc7d0b3
VitorFCM <vitor.carreramiguel@gmail.com>
2022-10-20 15:11:38 -0300

    Adicionando o tratamento de exceptions quando o o cliente tentar se conectar ao servidor

--- client.py
+++ client.py
@@ -12,7 +12,14 @@ def aguarda_mensagem(s):
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-s.connect(addr)
+try:
+    s.connect(addr)
+except socket.timeout:
+    print("O servidor demorou muito para responder.")
+    quit()
+except:
+    print("Ocorreu um problema durante a tentativa de conexao.")
+    quit()
 
 thread_aguarda_mensagem = threading.Thread(target = aguarda_mensagem, args=(s,))
 thread_aguarda_mensagem.start()
